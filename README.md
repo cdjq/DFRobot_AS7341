2c34f65adb30b31b8fbee66279e04964ebc9cd2c
pleasetakeup <you@example.com>
2020-07-16 16:20:30 +0800

    V1.0

---
+++ README.md
@@ -0,0 +1,52 @@
+# DFRobot_AS7341
+
+![正反面svg效果图](https://github.com/ouki-wang/DFRobot_Sensor/raw/master/resources/images/SEN0245svg1.png)
+
+## 产品链接（链接到英文商城）
+    SKU：LM75B 温度传感器
+   
+## Table of Contents
+
+* [Summary](#summary)
+* [Installation](#installation)
+* [Methods](#methods)
+* [Compatibility](#compatibility)
+* [History](#history)
+* [Credits](#credits)
+
+## Summary
+
+
+
+## Installation
+
+To use this library, first download the library file, paste it into the \Arduino\libraries directory, then open the examples folder and run the demo in the folder.
+
+## Methods
+
+
+## Compatibility
+
+MCU                | Work Well    | Work Wrong   | Untested    | Remarks
+------------------ | :----------: | :----------: | :---------: | -----
+Arduino Uno        |      √       |              |             | 
+Mega2560        |      √       |              |             | 
+Leonardo        |      √       |              |             | 
+ESP32        |      √       |              |             | 
+micro:bit        |      √       |              |             | 
+
+
+## History
+
+- data 2019-7-31
+- version V0.1
+
+
+## Credits
+
+Written by fengli(li.feng@dfrobot.com), 2019.7.31 (Welcome to our [website](https://www.dfrobot.com/))
+
+
+
+
+
---
+++ examples/getData/getData.ino
@@ -0,0 +1,75 @@
+/*!
+ * @file getData.ino
+ * @brief 读取光谱传感器10个光通道的数值，光源的某波长的光越多，对应通道的数值越大
+ * 
+ * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @licence     The MIT License (MIT)
+ * @author [fengli](li.feng@dfrobot.com)
+ * @version  V1.0
+ * @date  2020-07-16
+ * @get from https://www.dfrobot.com
+ * @url https://github.com/DFRobot/DFRobot_AS7341
+ */
+#include "DFRobot_AS7341.h"
+/*!
+ * @brief Construct the function
+ * @param pWire IC bus pointer object and construction device, can both pass or not pass parameters, Wire in default.
+ */
+DFRobot_AS7341 as7341;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  //检测IIC是否能正常通信
+  while (as7341.begin() != 0) {
+    Serial.println("IIC初始化失败，请检测连线是否正确");
+    delay(1000);
+  }
+  
+}
+void loop(void)
+{
+  DFRobot_AS7341::sModeOneData_t data1;
+  DFRobot_AS7341::sModeTwoData_t data2;
+  //设置寄存器ATIME的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
+  as7341.setAtime(100);
+  //设置ASTEP寄存器的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
+  as7341.setAstep(999);
+  //设置增益(0~10对应 X0.5,X1,X2,X4,X8,X16,X32,X64,X128,X256,X512)
+  as7341.setAGAIN(9);
+  //开始光谱的测量.
+  //通道映射的模式 :1.eF1F4ClearNIR,2.eF5F8ClearNIR
+  as7341.startMeasure(as7341.eF1F4ClearNIR);
+  //读取传感器数据通道0~5的值，eF1F4ClearNIR模式下.
+  data1 = as7341.readSpectralDataOne();
+  
+  Serial.print("ADC0/F1-(紫色) ");
+  Serial.println(data1.ADF1);
+  Serial.print("ADC1/F2-(靛)");
+  Serial.println(data1.ADF2);
+  Serial.print("ADC2/F3-(蓝紫)");
+  Serial.println(data1.ADF3);
+  Serial.print("ADC3/F4-(蓝)");   
+  Serial.println(data1.ADF4);
+  Serial.print("ADC4/Clear-(红外)");
+  Serial.println(data1.ADCLEAR);
+  Serial.print("ADC5/NIR-");
+  Serial.println(data1.ADNIR);
+  delay(1000);
+  as7341.startMeasure(as7341.eF5F8ClearNIR);
+  //读取传感器数据通道0~5的值,eF5F8ClearNIR模式下.
+  data2 = as7341.readSpectralDataTwo();
+  Serial.print("ADC0/F5-(绿色) ");
+  Serial.println(data2.ADF5);
+  Serial.print("ADC1/F6-(黄)");
+  Serial.println(data2.ADF6);
+  Serial.print("ADC2/F7-(橙)");
+  Serial.println(data2.ADF7);
+  Serial.print("ADC3/F8-(红)");   
+  Serial.println(data2.ADF8);
+  Serial.print("ADC4/Clear-(红外)");
+  Serial.println(data2.ADCLEAR);
+  Serial.print("ADC5/NIR-");
+  Serial.println(data2.ADNIR);
+  delay(1000);
+}
