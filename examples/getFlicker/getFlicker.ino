908851324c3e2be0386fe16661b4bd055c1d03ea
pleasetakeup <you@example.com>
2020-07-21 13:23:27 +0800

    v1.1

--- examples/getFlicker/getFlicker.ino
+++ examples/getFlicker/getFlicker.ino
@@ -1,6 +1,6 @@
 /*!
- * @file getFlicker.ino
- * @brief 读取光源的闪烁频率
+ * @file getData.ino
+ * @brief 读取光谱传感器10个光通道的数值，光源的某波长的光越多，对应通道的数值越大
  * 
  * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
@@ -17,7 +17,6 @@
  */
 DFRobot_AS7341 as7341;
 
-
 void setup(void)
 {
   Serial.begin(115200);
@@ -26,34 +25,59 @@ void setup(void)
     Serial.println("IIC初始化失败，请检测连线是否正确");
     delay(1000);
   }
-  
-}
-
-void loop(void){
-  uint8_t freq = 0;
+  //Integration time = (ATIME + 1) x (ASTEP + 1) x 2.78µs
   //设置寄存器ATIME的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
-  as7341.setAtime(100);
+  as7341.setAtime(29);
   //设置ASTEP寄存器的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
-  as7341.setAstep(999);
+  as7341.setAstep(599);
   //设置增益(0~10对应 X0.5,X1,X2,X4,X8,X16,X32,X64,X128,X256,X512)
-  as7341.setAGAIN(9);
+  as7341.setAGAIN(7);
+  //加上注释
+  //as7341.enableLed(true);
+  //as7341.controlLed(0);  
+
+}
+void loop(void)
+{
+  DFRobot_AS7341::sModeOneData_t data1;
+  DFRobot_AS7341::sModeTwoData_t data2;
+
+
+  //开始光谱的测量.
+  //通道映射的模式 :1.eF1F4ClearNIR,2.eF5F8ClearNIR
+  as7341.startMeasure(as7341.eF1F4ClearNIR);
+  //读取传感器数据通道0~5的值，eF1F4ClearNIR模式下.
+  data1 = as7341.readSpectralDataOne();
   
-  //读取flicker寄存器的值，可据此预计光源的闪烁频率
-  freq = as7341.readFlickerData();
-  //Serial.println(freq);
-  if (freq == 44) {
-    Serial.println("Unknown frequency");
+  Serial.print("F1(405-425nm):");
+  Serial.println(data1.ADF1);
+  Serial.print("F2(435-455nm):");
+  Serial.println(data1.ADF2);
+  Serial.print("F3(470-490nm):");
+  Serial.println(data1.ADF3);
+  Serial.print("F4(505-525nm):");   
+  Serial.println(data1.ADF4);
+  //Serial.print("ADC4/Clear-(红外)");
+  //Serial.println(data1.ADCLEAR);
+  //Serial.print("ADC5/NIR-");
+  //Serial.println(data1.ADNIR);
+  //delay(1000);
+  as7341.startMeasure(as7341.eF5F8ClearNIR);
+  //读取传感器数据通道0~5的值,eF5F8ClearNIR模式下.
+  data2 = as7341.readSpectralDataTwo();
+  Serial.print("F5(545-565nm):");
+  Serial.println(data2.ADF5);
+  Serial.print("F6(580-600nm):");
+  Serial.println(data2.ADF6);
+  Serial.print("F7(620-640nm):");
+  Serial.println(data2.ADF7);
+  Serial.print("F8(670-690nm):");   
+  Serial.println(data2.ADF8);
+  Serial.print("ADC4/Clear-(红外)");
+  Serial.println(data2.ADCLEAR);
+  Serial.print("ADC5/NIR-");
+  Serial.println(data2.ADNIR);
 
-  } else if (freq == 45) {
-    Serial.println("100 Hz ");
-
-  } else if (freq == 46) {
-    Serial.println("120 Hz ");
-  } else {
-    Serial.println("An unknown error");
-  }
- 
-
-
-
+  delay(1000);
+  
 }
