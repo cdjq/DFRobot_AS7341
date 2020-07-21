22887587f8288e8a99b7a1cba7c42deee4322489
e <1586254416@qq.com>
2020-07-21 13:47:07 +0800

    V1.2

--- examples/getData/getData.ino
+++ examples/getData/getData.ino
@@ -25,14 +25,17 @@ void setup(void)
     Serial.println("IIC初始化失败，请检测连线是否正确");
     delay(1000);
   }
+  //Integration time = (ATIME + 1) x (ASTEP + 1) x 2.78µs
   //设置寄存器ATIME的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
-  as7341.setAtime(100);
+  as7341.setAtime(29);
   //设置ASTEP寄存器的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
-  as7341.setAstep(999);
+  as7341.setAstep(599);
   //设置增益(0~10对应 X0.5,X1,X2,X4,X8,X16,X32,X64,X128,X256,X512)
-  as7341.setAGAIN(9);
-  as7341.enableLed(true);
-  as7341.controlLed(0);  
+  as7341.setAGAIN(7);
+  //使能LED
+  //as7341.enableLed(true);
+  //设置引脚电流控制亮度(0~20对应电流 4mA,6mA,8mA,10mA,12mA,......,42mA)
+  //as7341.controlLed(0);
 
 }
 void loop(void)
@@ -47,13 +50,13 @@ void loop(void)
   //读取传感器数据通道0~5的值，eF1F4ClearNIR模式下.
   data1 = as7341.readSpectralDataOne();
   
-  Serial.print("ADC0/F1-(紫色) ");
+  Serial.print("F1(405-425nm):");
   Serial.println(data1.ADF1);
-  Serial.print("ADC1/F2-(靛)");
+  Serial.print("F2(435-455nm):");
   Serial.println(data1.ADF2);
-  Serial.print("ADC2/F3-(蓝紫)");
+  Serial.print("F3(470-490nm):");
   Serial.println(data1.ADF3);
-  Serial.print("ADC3/F4-(蓝)");   
+  Serial.print("F4(505-525nm):");   
   Serial.println(data1.ADF4);
   //Serial.print("ADC4/Clear-(红外)");
   //Serial.println(data1.ADCLEAR);
@@ -63,19 +66,17 @@ void loop(void)
   as7341.startMeasure(as7341.eF5F8ClearNIR);
   //读取传感器数据通道0~5的值,eF5F8ClearNIR模式下.
   data2 = as7341.readSpectralDataTwo();
-  Serial.print("ADC0/F5-(绿色) ");
+  Serial.print("F5(545-565nm):");
   Serial.println(data2.ADF5);
-  Serial.print("ADC1/F6-(黄)");
+  Serial.print("F6(580-600nm):");
   Serial.println(data2.ADF6);
-  Serial.print("ADC2/F7-(橙)");
+  Serial.print("F7(620-640nm):");
   Serial.println(data2.ADF7);
-  Serial.print("ADC3/F8-(红)");   
+  Serial.print("F8(670-690nm):");
   Serial.println(data2.ADF8);
-  Serial.print("ADC4/Clear-(红外)");
+  Serial.print("Clear:");
   Serial.println(data2.ADCLEAR);
-  Serial.print("ADC5/NIR-");
+  Serial.print("NIR:");
   Serial.println(data2.ADNIR);
-
   delay(1000);
-  
 }
