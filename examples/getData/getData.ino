9230c9d4ca1fa1bfd8dc8b27a5bb3d8e129c7748
LZY1586 <53321447+LZY1586@users.noreply.github.com>
2020-08-24 17:32:48 +0800

    Update getData.ino

--- examples/getData/getData.ino
+++ examples/getData/getData.ino
@@ -58,9 +58,9 @@ void loop(void)
   Serial.println(data1.ADF3);
   Serial.print("F4(505-525nm):");   
   Serial.println(data1.ADF4);
-  //Serial.print("ADC4/Clear-(红外)");
+  //Serial.print("Clear:");
   //Serial.println(data1.ADCLEAR);
-  //Serial.print("ADC5/NIR-");
+  //Serial.print("NIR:");
   //Serial.println(data1.ADNIR);
   //delay(1000);
   as7341.startMeasure(as7341.eF5F8ClearNIR);
