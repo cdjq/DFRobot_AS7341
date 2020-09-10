d0c777c3c4de47bd8e31cc92e349f73c8ca36e90
Frances9 <44355133+Frances9@users.noreply.github.com>
2020-09-10 11:16:38 +0800

    Update getData.ino

--- examples/getData/getData.ino
+++ examples/getData/getData.ino
@@ -1,7 +1,8 @@
 /*!
  * @file getData.ino
- * @brief 读取光谱传感器10个光通道的数值，光源的某波长的光越多，对应通道的数值越大
- * 
+ * @brief Read the values of 10 optical channels of the AS7341 spectral sensor, the more light of a certain wavelength of the light source,
+ * the greater the corresponding channel value.
+ *
  * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
  * @author [fengli](li.feng@dfrobot.com)
@@ -20,21 +21,22 @@ DFRobot_AS7341 as7341;
 void setup(void)
 {
   Serial.begin(115200);
-  //检测IIC是否能正常通信
+  //Detect if IIC can communicate properly 
   while (as7341.begin() != 0) {
-    Serial.println("IIC初始化失败，请检测连线是否正确");
+    Serial.println("IIC init failed, please check if the wire connection is correct");
     delay(1000);
   }
   //Integration time = (ATIME + 1) x (ASTEP + 1) x 2.78µs
-  //设置寄存器ATIME的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
+  //Set the value of register ATIME, through which the value of Integration time can be calculated. The value represents the time that must be spent during data reading.
   as7341.setAtime(29);
-  //设置ASTEP寄存器的值，通过该值可计算Integration time的值，该值表示读取数据过程中必须要消耗的时间
+  //Set the value of register ASTEP, through which the value of Integration time can be calculated. The value represents the time that must be spent during data reading.
+   * @ the time that must be spent during data reading.
   as7341.setAstep(599);
-  //设置增益(0~10对应 X0.5,X1,X2,X4,X8,X16,X32,X64,X128,X256,X512)
+  //Set gain value(0~10 corresponds to X0.5,X1,X2,X4,X8,X16,X32,X64,X128,X256,X512)
   as7341.setAGAIN(7);
-  //使能LED
+  //Enable LED
   //as7341.enableLed(true);
-  //设置引脚电流控制亮度(1~20对应电流 4mA,6mA,8mA,10mA,12mA,......,42mA)
+  //Set pin current to control brightness (1~20 corresponds to current 4mA,6mA,8mA,10mA,12mA,......,42mA)
   //as7341.controlLed(0);
 
 }
@@ -44,10 +46,10 @@ void loop(void)
   DFRobot_AS7341::sModeTwoData_t data2;
 
 
-  //开始光谱的测量.
-  //通道映射的模式 :1.eF1F4ClearNIR,2.eF5F8ClearNIR
+  //Start spectrum measurement 
+  //Channel mapping mode: 1.eF1F4ClearNIR,2.eF5F8ClearNIR
   as7341.startMeasure(as7341.eF1F4ClearNIR);
-  //读取传感器数据通道0~5的值，eF1F4ClearNIR模式下.
+  //Read the value of sensor data channel 0~5, under eF1F4ClearNIR
   data1 = as7341.readSpectralDataOne();
   
   Serial.print("F1(405-425nm):");
@@ -58,13 +60,13 @@ void loop(void)
   Serial.println(data1.ADF3);
   Serial.print("F4(505-525nm):");   
   Serial.println(data1.ADF4);
-  //Serial.print("ADC4/Clear-(红外)");
+  //Serial.print("ADC4/Clear-(IR)");
   //Serial.println(data1.ADCLEAR);
   //Serial.print("ADC5/NIR-");
   //Serial.println(data1.ADNIR);
   //delay(1000);
   as7341.startMeasure(as7341.eF5F8ClearNIR);
-  //读取传感器数据通道0~5的值,eF5F8ClearNIR模式下.
+  //Read the value of sensor data channel 0~5, under eF5F8ClearNIR
   data2 = as7341.readSpectralDataTwo();
   Serial.print("F5(545-565nm):");
   Serial.println(data2.ADF5);
